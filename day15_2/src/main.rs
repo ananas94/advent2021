4968bbaeaf71af4c29adf97afe08c2aac5027986
ananas94 <den.ananas94@gmail.com>
2021-12-15 15:51:05 +0300

    day15

---
+++ day15_2/src/main.rs
@@ -0,0 +1,125 @@
+use std::io;
+use std::error::Error;
+use std::collections::BinaryHeap;
+use std::cmp::Ordering;
+
+//from day 9 begin 
+fn get_input() -> Option<Vec<u8>> {
+    let mut inp = String::new();
+    io::stdin().read_line(&mut inp).ok()?;
+    let numbers: Vec<u8> = inp.trim().split("").skip(1).
+                            map(|x| x.trim().parse()).
+                            take_while(|x| x.is_ok()).
+                            map(|x| x.unwrap()).collect();
+    if numbers.len() == 0  {return None; }
+    Some(numbers)
+}
+
+fn get_matrix() -> Option<Vec<Vec<u8>>> {
+    let mut ret = Vec::new();
+
+    let line = get_input()?;
+    let init_len = line.len();
+    ret.push(line);
+    while let Some(line) = get_input() {
+        let len = line.len();
+        if len != init_len { return None; }
+        ret.push(line);
+    }
+    Some(ret)
+}
+// from day9 end
+
+fn step(x: usize, y:usize, dx: usize, dy:usize, cave: &Vec<Vec<u8>>, distances: &mut Vec<Vec<u64>>,  queue: &mut BinaryHeap<DCpair>) {
+    if (distances[x][y] + (cave[dx][dy] as u64)) < distances[dx][dy] {
+        distances[dx][dy] = distances[x][y] + (cave[dx][dy] as u64);
+
+        let dc = DCpair{ 
+            distance: distances[dx][dy],
+            x : dx,
+            y: dy
+        };
+        queue.push(dc);
+    }
+
+}
+
+
+#[derive(Eq,PartialEq)]
+struct DCpair{
+    distance: u64,
+    x: usize,
+    y: usize
+}
+
+impl Ord for DCpair {
+        fn cmp(&self, other: &Self) -> Ordering {
+            other.distance.cmp(&self.distance)
+        }
+}
+
+impl PartialOrd for DCpair {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn extend(cave: &Vec<Vec<u8>>) -> Vec<Vec<u8>> {
+    let mut c: Vec<Vec<u8>> = vec![vec![0; cave.len() * 5]; cave[0].len() * 5];
+    let dx = cave.len();
+    let dy = cave[0].len();
+     
+
+    for i in 0..5 as usize {
+        for j in 0..5 as usize {
+            for ik in 0..dx {
+                for jk in 0..dy {
+                    let val = cave[ik][jk] + i as u8 + j as u8;
+                    let dv = val/10;
+                    let mv = val%10;
+                    c[dx*i+ik][dy*j+jk] = dv + mv;
+                }
+            }
+        }
+    }
+    c
+}
+
+
+fn main() ->Result<(), Box<dyn Error>> {
+    let itintal_cave = get_matrix().ok_or("bad_inp")?;
+
+    let cave = extend(&itintal_cave);
+
+    println!("start");
+
+    let mut distances: Vec<Vec<u64>>= vec!( vec![u64::MAX; cave.len()]; cave[0].len() );
+
+    let mut queue: BinaryHeap<DCpair> = BinaryHeap::new();
+
+    distances[0][0] = 0;
+    queue.push(DCpair{distance:0,x:0,y:0});
+
+    while let Some(h) = queue.pop() {
+        let x = h.x;
+        let y = h.y;
+        if x>0 {
+            step(x,y,x-1,y,&cave,&mut distances, &mut queue);
+        }
+        if y>0
+        {
+            step(x,y,x,y-1,&cave,&mut distances, &mut queue);
+        }
+        if x<cave.len()-1 {
+            step(x,y,x+1,y,&cave,&mut distances, &mut queue);
+        }
+        if y<cave[0].len()-1 {
+            step(x,y,x,y+1,&cave,&mut distances, &mut queue);
+        }
+        if  distances[distances.len() -1 ][distances[0].len()-1] != u64::MAX { break;}
+    }
+    println!("{}", distances[distances.len() -1 ][distances[0].len()-1]);
+
+
+    Ok(())
+}
